384e5928d8b8f1bb0edac45b17a52e3e501502a7
enockmagara <emm10042@nyu.edu>
2025-03-24 21:17:30 +0400

    Fixed security

--- server/app.py
+++ server/app.py
@@ -26,4 +26,4 @@ api = Api(app)
 api.add_namespace(students)  # Add the students namespace to the API
 
 if __name__ == "__main__":
-    app.run(debug=True, port=6969, host="0.0.0.0")
+    app.run(debug=False, port=6969, host="127.0.0.1")
